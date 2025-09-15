70005779d5614626364d04f2319745a8900eafac
Tsue_Hic22009 <hic22009@byui.edu>
2025-09-15 11:55:27 -0500

    Lesson 1 assignment code - added

---
+++ server.js
@@ -0,0 +1,12 @@
+// express web server
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hannah Birch');
+});
+
+const port = 3000;
+
+app.listen(process.env.port || port);
+console.log('Web Server is listening at port ' + (process.env.port || 3000));
